3f75be69f02bbc53d335b85e46ad8ea888e938fc
Anthony <0xsoto@gmail.com>
2017-07-27 13:33:54 -0400

    local emacs config for project

---
+++ .dir-locals.el
@@ -0,0 +1,13 @@
+((c-mode
+  (flycheck-gcc-args . ("-mmcu=atmega32m1" "-Os" "-Wall"))
+  (flycheck-c/c++-gcc-executable . "/usr/bin/avr-gcc")
+  (flycheck-disabled-checkers . (c/c++-clang irony))
+  ))
+
+(
+ (nil . ((company-clang-arguments . ("-I/home/anthony/WorkSpace/Projects/bow_clicker/"
+ 				   ))))
+ )
+
+
+
